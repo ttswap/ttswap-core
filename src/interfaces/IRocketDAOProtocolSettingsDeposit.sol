428c85caf0852281bf8f01b9dfaf0a6c87656ba9
macbook m3 <ttswap.exchange@gmail.com>
2025-08-20 23:37:52 +0800

    remove unuse file 20250820
    Signed-off-by: macbook m3 <ttswap.exchange@gmail.com>

--- src/interfaces/IRocketDAOProtocolSettingsDeposit.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
-
-interface IRocketDAOProtocolSettingsDeposit {
-    function getDepositEnabled() external view returns (bool);
-    function getMaximumDepositPoolSize() external view returns (uint256);
-}
--- src/interfaces/IRocketDepositPool.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
-
-interface IRocketDepositPool {
-    function deposit() external payable;
-    function getBalance() external view returns (uint256);
-}
