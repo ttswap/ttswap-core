518b9bc1c567ce0965dcfafdca160ad5656a11d5
macbook m3 <ttswap.exchange@gmail.com>
2025-09-11 15:22:37 +0800

    format the file 20250911
    Signed-off-by: macbook m3 <ttswap.exchange@gmail.com>

--- src/TTSwap_Token_Proxy.sol
+++ src/TTSwap_Token_Proxy.sol
@@ -3,9 +3,7 @@ pragma solidity 0.8.29;
 
 import {TTSwapError} from "./libraries/L_Error.sol";
 import {L_UserConfigLibrary} from "./libraries/L_UserConfig.sol";
-import {
-    toTTSwapUINT256
-} from "./libraries/L_TTSwapUINT256.sol";
+import {toTTSwapUINT256} from "./libraries/L_TTSwapUINT256.sol";
 /**
  * @title TTSwap_Market
  * @dev Core market contract for TTSwap protocol that manages goods trading, investing, and staking operations
@@ -19,11 +17,11 @@ import {
  */
 contract TTSwap_Token_Proxy {
     using L_UserConfigLibrary for uint256;
-    string internal name     ;
-    string internal symbol ;
+    string internal name;
+    string internal symbol;
     string internal totalSupply;
     mapping(address => uint256) internal balanceOf;
-    mapping(address => mapping(address => uint256))  internal allowance;
+    mapping(address => mapping(address => uint256)) internal allowance;
     mapping(address => uint256) internal nonces;
     address internal implementation;
     bool internal upgradeable;
@@ -31,53 +29,62 @@ contract TTSwap_Token_Proxy {
     uint256 internal ttstokenconfig;
     uint256 internal stakestate;
     uint128 internal left_share = 45_000_000_000_000;
-    uint128 internal  publicsell ;
+    uint128 internal publicsell;
     mapping(address => uint256) internal userConfig;
 
     event e_updateUserConfig(address user, uint256 config);
     constructor(
-       address _usdt, address _dao_admin, uint256 _ttsconfig,string memory _name,string memory _symbol,address _implementation
+        address _usdt,
+        address _dao_admin,
+        uint256 _ttsconfig,
+        string memory _name,
+        string memory _symbol,
+        address _implementation
     ) {
         usdt = _usdt;
         stakestate = toTTSwapUINT256(uint128(block.timestamp), 0);
         ttstokenconfig = _ttsconfig;
-        userConfig[_dao_admin]=userConfig[_dao_admin].setDAOAdmin(true);
-        name =_name;
-        symbol=_symbol;
-        implementation=_implementation;
-        upgradeable=true;
-        emit e_updateUserConfig(_dao_admin,userConfig[_dao_admin]);
+        userConfig[_dao_admin] = userConfig[_dao_admin].setDAOAdmin(true);
+        name = _name;
+        symbol = _symbol;
+        implementation = _implementation;
+        upgradeable = true;
+        emit e_updateUserConfig(_dao_admin, userConfig[_dao_admin]);
     }
 
-    fallback() external payable{
+    fallback() external payable {
         address impl = implementation;
         assembly {
             calldatacopy(0, 0, calldatasize())
             let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
             returndatacopy(0, 0, returndatasize())
-            if iszero(result) { revert(0, returndatasize()) }
+            if iszero(result) {
+                revert(0, returndatasize())
+            }
             return(0, returndatasize())
         }
     }
 
-        /// onlydao admin can execute
+    /// onlydao admin can execute
     modifier onlyTokenAdminProxy() {
-        if (!userConfig[msg.sender].isTokenAdmin()||!upgradeable) revert TTSwapError(1);
+        if (!userConfig[msg.sender].isTokenAdmin() || !upgradeable)
+            revert TTSwapError(1);
         _;
     }
 
     /// onlydao admin can execute
     modifier onlyTokenOperatorProxy() {
-        if (!userConfig[msg.sender].isTokenManager()||!upgradeable) revert TTSwapError(1);
+        if (!userConfig[msg.sender].isTokenManager() || !upgradeable)
+            revert TTSwapError(1);
         _;
     }
 
-    function upgrade(address _implementation) external onlyTokenAdminProxy{
-        implementation=_implementation;
+    function upgrade(address _implementation) external onlyTokenAdminProxy {
+        implementation = _implementation;
     }
 
-    function freezeToken() external onlyTokenOperatorProxy{
-        implementation=address(0);
+    function freezeToken() external onlyTokenOperatorProxy {
+        implementation = address(0);
     }
 
     receive() external payable {}
